91d2a4286afbf1b7f63347019d2e581201bf95e4
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2021-09-11 15:43:13 -0300

    pequenas mudanças

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BankListService } from 'src/components/template/service/bankList/bank-list.service';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +6,6 @@ import { BankListService } from 'src/components/template/service/bankList/bank-l
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  bancos: any
-
-  constructor(private bankListService: BankListService) {}
-
-  ngOnInit(): void {
-    this.ListarBancos();
-  }
-  ListarBancos() {
-    this.bankListService.listBank().subscribe(
-      (bancos) => {
-        this.bancos= bancos
-      },
-      (err) => {
-        console.log('erro ao listar os bancos', err);
-      }
-    );
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
