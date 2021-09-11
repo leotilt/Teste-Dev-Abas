e9dde673e5808da62681ab08dae18c0935a721a2
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2021-09-11 15:42:37 -0300

    add interface no service

---
+++ frontend/src/components/models/bancos.ts
@@ -0,0 +1,6 @@
+export interface Bancos {
+  ispb: number;
+  name: string;
+  code: number;
+  fullName: string;
+}
--- frontend/src/components/template/service/bankList/bank-list.service.ts
+++ frontend/src/components/template/service/bankList/bank-list.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Bancos } from '../../../models/bancos';
 @Injectable({
   providedIn: 'root',
 })
 export class BankListService {
   constructor(private http: HttpClient) {}
 
-  listBank(): Observable<any> {
-    return this.http.get('https://brasilapi.com.br/api/banks/v1');
+  getBanks(): Observable<any> {
+    return this.http.get<Bancos>('https://brasilapi.com.br/api/banks/v1');
   }
 }
