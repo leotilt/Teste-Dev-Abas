0b4d1c8ff5be6739b37e32e2e6f8e4bd46e1ac24
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2021-09-14 19:16:11 -0300

    algumas alteraçoes no bankService

--- frontend/src/components/template/service/bankList/bank-list.service.ts
+++ frontend/src/components/template/service/bankList/bank-list.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Bancos } from '../../../models/bancos';
+
 @Injectable({
   providedIn: 'root',
 })
 export class BankListService {
   constructor(private http: HttpClient) {}
 
-  getBanks(): Observable<any> {
-    return this.http.get<Bancos>('https://brasilapi.com.br/api/banks/v1');
+  getBanks(): Observable<Bancos[]> {
+    return this.http.get<Bancos[]>('https://brasilapi.com.br/api/banks/v1')
+    
   }
 }
