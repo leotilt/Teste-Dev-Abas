d84f78294eed2e0c104b01597ebbf0da1fe2d7a6
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2021-09-14 19:15:06 -0300

    pagina inicial/read localStorage

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { Form } from 'src/components/models/form';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  displayedColumns: string[] = ['banco', 'agencia', 'conta'];
+
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+
+  data!: Form[];
+  itemDelete: any;
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.readLocalStorage();
+  }
+
+  clearLocalStorage() {
+    if (confirm('Tem certeza que deseja deletar todas as contas?')) {
+      localStorage.clear();
+      window.location.reload();
+      this.snackBar.open('Todas as contas deletadas com sucesso', 'X', {
+        duration: 2000,
+        horizontalPosition: 'right',
+        verticalPosition: 'bottom',
+      });
+    }
+  }
+
+  readLocalStorage() {
+    this.data = JSON.parse(localStorage.getItem('form-data') as string);
+  }
 }
