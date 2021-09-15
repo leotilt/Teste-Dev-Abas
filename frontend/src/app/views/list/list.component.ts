45e2ee905d392fffcc5cfeeb04766b15508f46d9
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2021-09-15 00:02:25 -0300

    dataSource/filter/paginator/sort

--- frontend/src/app/views/list/list.component.ts
+++ frontend/src/app/views/list/list.component.ts
@@ -1,38 +1,50 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { BankListService } from 'src/components/template/service/bankList/bank-list.service';
 import { MatSort } from '@angular/material/sort';
 import { Bancos } from 'src/components/models/bancos';
+import { UtilService } from 'src/components/template/service/utils/util.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit, AfterViewInit {
   //Table
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
 
   displayedColumns: string[] = ['code', 'name'];
-  
-  dataSource =  new MatTableDataSource()
-  ListasBancos: Bancos[] = [];
+  dataSource = new MatTableDataSource<Bancos>();
 
-  constructor(private bankListService: BankListService) {}
+  constructor(
+    private bankListService: BankListService,
+    private utils: UtilService
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit(): void {
-    this.getListBanks();
-    // this.dataSource =  new MatTableDataSource(this.ListasBancos);
+    this.updateDataSource();
   }
-  getListBanks() {
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  updateDataSource() {
     this.bankListService.getBanks().subscribe(
-      (bancos) => {
-        this.ListasBancos = bancos;
+      (data) => {
+        this.dataSource.data = data;
       },
       (err) => {
-        console.log('erro ao listar os bancos', err);
+        this.utils.errorMessage('Erro ao Listar bancos' + err);
       }
     );
   }
